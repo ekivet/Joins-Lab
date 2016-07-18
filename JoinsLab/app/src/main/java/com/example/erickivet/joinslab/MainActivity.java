2731a4391950c931397a1f3f2e704c7a895c9aaf
eric.kivet <eric.kivet@gmail.com>
2016-07-18 02:09:32 -0400

    commit #2

--- JoinsLab/app/src/main/java/com/example/erickivet/joinslab/MainActivity.java
+++ JoinsLab/app/src/main/java/com/example/erickivet/joinslab/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.erickivet.joinslab;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +18,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DatabaseHelper helper = DatabaseHelper.getInstance(MainActivity.this);
+
+        Employee employee1 = new Employee("123-04-5678","John","Smith",1973,"NY");
+        Employee employee2 = new Employee("123-04-5679","David","McWill",1982,"Seattle");
+        Employee employee3 = new Employee("123-04-5680","Katerina","Wise",1973,"Boston");
+        Employee employee4 = new Employee("123-04-5681","Donald","Lee",1992,"London");
+        Employee employee5 = new Employee("123-04-5682","Gary","Henwood",1987,"Las Vegas");
+        Employee employee6 = new Employee("123-04-5683","Anthony","Bright",1963,"Seattle");
+        Employee employee7 = new Employee("123-04-5684","William","Newey",1995,"Boston");
+        Employee employee8 = new Employee("123-04-5685","Melony","Smith",1970,"Chicago");
+
+        Job job1 = new Job("123-04-5678","Fuzz",60,1);
+        Job job2 = new Job("123-04-5679","GA",70,2);
+        Job job3 = new Job("123-04-5680","Little Place",120,5);
+        Job job4 = new Job("123-04-5681","Macy's",78,3);
+        Job job5 = new Job("123-04-5682","New Life",65,1);
+        Job job6 = new Job("123-04-5683","Believe",158,6);
+        Job job7 = new Job("123-04-5684","Macy's",200,8);
+        Job job8 = new Job("123-04-5685","Stop",299,12);
+
+        helper.insertRowEmployee(employee1);
+        helper.insertRowEmployee(employee2);
+        helper.insertRowEmployee(employee3);
+        helper.insertRowEmployee(employee4);
+        helper.insertRowEmployee(employee5);
+        helper.insertRowEmployee(employee6);
+        helper.insertRowEmployee(employee7);
+        helper.insertRowEmployee(employee8);
+
+        helper.insertRowJob(job1);
+        helper.insertRowJob(job2);
+        helper.insertRowJob(job3);
+        helper.insertRowJob(job4);
+        helper.insertRowJob(job5);
+        helper.insertRowJob(job5);
+        helper.insertRowJob(job6);
+        helper.insertRowJob(job7);
+        helper.insertRowJob(job8);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
